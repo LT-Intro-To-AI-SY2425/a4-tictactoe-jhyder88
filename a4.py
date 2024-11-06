b41745b7165b640c942a9ba6cc897dfd3ea9095b
Joun Hyder <joun88hyder@gmail.com>
2024-11-06 11:43:00 -0600

    updated a4

--- a4.py
+++ a4.py
@@ -10,9 +10,48 @@ class TTTBoard:
             represent moves by player 'O' and '*'s are spots no one has yet played on
     """
 
-    pass
+    def __init__(self) -> None:
+        self.board = ['*'] * 9
+
+    
+    def __str__(self) -> str:
+        s = ""
+        for x in [0, 3, 6]:
+            s += self.board[x + 0] + " " + self.board[x + 1] + " " + self.board[x + 2] + "\n"
+        return s
+    
 
 
+    def make_move(self, player, pos) -> bool:
+        if pos < 0 or pos > 8 or self.board[pos] != '*':
+            return False
+        
+        self.board[pos] = player
+        return True
+    
+    def has_won(self, player) -> bool:
+        ps = [player] * 3 #either [X, X, X] or [O, O, O]
+
+        #checks horizontal
+        if self.board[:3] == ps or self.board[3:6] == ps or self.board[6:] == ps:
+            return True
+        
+
+        def game_over(self) -> bool:
+            if self.has_won("X") or self.has_won("O") or "*" not in self.board:
+                return True
+            
+            return False
+
+            if self.board[::3] == ps or self.board[1::3] == ps or self.board[2::3] == ps:
+                return True
+            
+            if self.board[::4] == ps or self.board[2:7:2] == ps:
+                return True
+        
+
+        def clear(self) -> None:
+            self.board = ["*"] * 9
 def play_tic_tac_toe() -> None:
     """Uses your class to play TicTacToe"""
 
